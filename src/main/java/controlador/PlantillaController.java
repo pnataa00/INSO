e962ea56ace81f2b3e3424ca36629acee31b087c
pnataa00 <pnataa00@estudiantes.unileon.es>
2024-05-19 19:51:01 +0200

    avance

--- src/main/java/controlador/PlantillaController.java
+++ src/main/java/controlador/PlantillaController.java
@@ -5,10 +5,26 @@
  */
 package controlador;
 
+import java.io.IOException;
+import java.io.Serializable;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
 /**
  *
  * @author pablo
  */
-public class PlantillaController {
+
+@Named
+@ViewScoped
+public class PlantillaController implements Serializable{
+    
+    public void verificarYMostrar() throws IOException {
+        Object us = FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario");
+        if(us==null){
+            FacesContext.getCurrentInstance().getExternalContext().redirect(FacesContext.getCurrentInstance().getExternalContext().getApplicationContextPath()+"/publico/permisosinsuficientes.xhtml?faces-redirect=true");
+        }
+    }
     
 }
